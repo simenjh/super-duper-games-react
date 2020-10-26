88b20a3025c8a09430f1952d8d64c9a8173f25fc
Simen Janbu Høgseth <simenjh1@gmail.com>
2020-10-26 15:26:40 +0100

    Improve responsiveness of home and games pages

--- app/client/bundles/games/games_list.js
+++ app/client/bundles/games/games_list.js
@@ -5,25 +5,45 @@ const _ = require("lodash");
 
 
 export class GamesList extends Component {
-  createImageComponents(){
-      let images = []
-      this.props.images.forEach((image) => {
-        images.push(
-                    <a href={"/games/" + image.game_id}>
-                      <ImageComponent image={{ src: image.url, width: this.props.image_width }} />
-                    </a>
-        );
-      });
-      return images;
+  state = {
+    n_images_in_row: window.innerWidth <= 419 ? 1 : this.props.n_images_in_row,
+    image_width: this.props.image_width ? this.props.image_width : "200px"
   }
 
-    wrapImages(images){
-        const size = this.props.n_images_in_row;
-        const grouped_images = _.chunk(images, size);
-        const wrapped_images = grouped_images.map((e) => {
-          return <div className="featured-games-row">{e}</div>
-        });
-        return wrapped_images;
+  updateDimensions = () => {
+    if(window.innerWidth <= 419 && (this.state.n_images_in_row != 1)) {
+      this.setState({ n_images_in_row: 1 });
+    }
+    else if(window.innerWidth > 419 && (this.state.n_images_in_row != this.props.n_images_in_row)) {
+      this.setState({ n_images_in_row: this.props.n_images_in_row });
+    }
+  };
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  createImageComponents(){
+    let images = []
+    this.props.images.forEach((image) => {
+      images.push(
+                  <a href={"/games/" + image.game_id}>
+                    <ImageComponent image={{ src: image.url, width: this.state.image_width }} />
+                  </a>
+      );
+    });
+    return images;
+  }
+
+  wrapImages(images){
+    const size = this.state.n_images_in_row;
+    const grouped_images = _.chunk(images, size);
+    const wrapped_images = grouped_images.map((e) => {
+      return <div className="featured-games-row">{e}</div>
+    });
+    return wrapped_images;
     }
 
   render(){
@@ -35,13 +55,4 @@ export class GamesList extends Component {
           </div>
     );
   }
-}
-
-
-
-
-
-GamesList.defaultProps = {
-  image_width: "200px",
-  n_images_in_row: 3
 }
